b3eca857bf0fff0a748d507cca2e3acedcc3570a
moneroist <moneroist@patronero.com>
2022-04-14 22:23:22 +0200

    Simple system tray with toggle show/hide and quit options

--- main.js
+++ main.js
@@ -1,17 +1,71 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron')
 const path = require('path')
 
+let browserWindow, tray
+  
+const quitApp = () => {
+  app.isQuit = true
+  app.quit()
+}
+
+const trayContextMenuTemplate = [
+  { label: 'Hide', click: () => { browserWindow.hide() } },
+  { label: 'Quit', click: () => { quitApp() } }
+]
+
+const createTray = () => {
+  tray = new Tray('logo.png')
+  const trayContextMenu = Menu.buildFromTemplate(trayContextMenuTemplate)
+  tray.setContextMenu(trayContextMenu)
+}
+
+const updateTray = (args) => {
+  const { visible } = args
+
+  if (visible != null) {
+    if (visible) {
+      trayContextMenuTemplate[0] = { label: 'Hide', click: () => { browserWindow.hide() } }
+    } else {
+      trayContextMenuTemplate[0] = { label: 'Show', click: () => { browserWindow.show() } }
+    }
+  }
+
+  const trayContextMenu = Menu.buildFromTemplate(trayContextMenuTemplate)
+  tray.setContextMenu(trayContextMenu)
+}
+
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  browserWindow = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  mainWindow.loadFile('src/index.html')
+  browserWindow.loadFile('src/index.html')
+
+  browserWindow.on('minimize', (event) => {
+    event.preventDefault()
+    browserWindow.hide()
+  })
+
+  browserWindow.on('close', (event) => {
+    if (!app.isQuit) {
+      event.preventDefault()
+      browserWindow.hide()
+    }
+  })
+
+  browserWindow.on('show', () => {
+    updateTray({ visible: true })
+  })
+
+  browserWindow.on('hide', (event) => {
+    updateTray({ visible: false })
+  })
 }
 
 app.whenReady().then(() => {
   createWindow()
+  createTray()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
